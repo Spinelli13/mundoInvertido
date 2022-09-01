a45b18f386a1566b21e8f6472e61f178c5f34b05
Spinelli13 <matheus.cspinelli@gmail.com>
2022-08-31 22:26:36 -0300

    Iniciado bando de dados Firebase e a interação com o formulario

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCq8dXg6nMK7vBurnJC6xsbmPONx4epozU",
+    authDomain: "mundo-invertido-dd.firebaseapp.com",
+    projectId: "mundo-invertido-dd",
+    storageBucket: "mundo-invertido-dd.appspot.com",
+    messagingSenderId: "502388363480",
+    appId: "1:502388363480:web:c7bfca0d575356e61c07ad"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  export default app;
